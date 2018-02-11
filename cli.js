49213cf7261de2a01029d4f5fdd4a5819f8c44d3
Ryan Zimmerman <opensrc@ryanzim.com>
2018-02-10 19:42:24 -0500

    BREAKING: Drop Node.js 4 support
    - Upgrade inquirer
    - Use prettier + eslint

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 'use strict';
-var fs = require('fs-extra');
-var inquirer = require('inquirer');
-var findPkg = require('pkg-up');
+const fs = require('fs-extra');
+const inquirer = require('inquirer');
+const findPkg = require('pkg-up');
 
-var script = process.argv[2];
+let script = process.argv[2];
 
 // help message:
 if (process.argv[2] === '--help') {
@@ -17,39 +17,40 @@ if (process.argv[2] === '--help') {
   process.exit();
 }
 
-var pkgPath;
-var pkg = {};
-var scripts = {};
+let pkgPath;
+let pkg = {};
+let scripts = {};
 
 const NEW_SCRIPT_SYMBOL = Symbol('Create new script');
 const EXIT_SYMBOL = Symbol('Exit');
 
 // Find package.json path:
 findPkg()
-  .then(function (p) {
+  .then(p => {
     if (!p) throw new Error('No package.json file found!');
     pkgPath = p;
     // Load package.json:
     return fs.readJson(pkgPath);
   })
-  .then(function (data) {
+  .then(data => {
     // Assign global variables:
     pkg = data;
     if (!pkg.scripts) pkg.scripts = {};
     scripts = pkg.scripts;
   })
   .then(getScriptName)
-  .then(function editScript() {
-    return inquirer.prompt([
-      {
-        type: 'editor',
-        name: 'script',
-        message: 'Edit your script; an empty script deletes the script',
-        default: scripts[script],
-      },
-    ])
-      .then(function (answers) {
-        var val = answers.script.trim();
+  .then(() => {
+    return inquirer
+      .prompt([
+        {
+          type: 'editor',
+          name: 'script',
+          message: 'Edit your script; an empty script deletes the script',
+          default: scripts[script],
+        },
+      ])
+      .then(answers => {
+        const val = answers.script.trim();
         if (!val) {
           console.log('Deleting script.');
           delete scripts[script];
@@ -57,7 +58,7 @@ findPkg()
         return fs.writeJson(pkgPath, pkg, { spaces: 2 });
       });
   })
-  .catch(function (err) {
+  .catch(err => {
     console.error(err);
     process.exit(1);
   });
@@ -68,7 +69,7 @@ function getScriptName() {
     return;
   }
   // Else, Get choices:
-  var choices = Object.keys(scripts).map(function (key) {
+  const choices = Object.keys(scripts).map(key => {
     return {
       name: pad(key, scripts[key]),
       value: key,
@@ -86,48 +87,53 @@ function getScriptName() {
     value: EXIT_SYMBOL,
   });
   // Prompt for script name:
-  return inquirer.prompt([
-    {
-      type: 'list',
-      name: 'script',
-      message: 'Select a script to edit:',
-      choices: choices,
-    },
-  ])
-    .then(function (answers) {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'script',
+        message: 'Select a script to edit:',
+        choices,
+      },
+    ])
+    .then(answers => {
       switch (answers.script) {
-      case NEW_SCRIPT_SYMBOL:
-        // Get script name:
-        return inquirer.prompt([{
-          type: 'input',
-          name: 'name',
-          message: 'Enter the script name:',
-          validate: function (val) {
-            if (!val) return 'Script name must not be empty';
-            else return true;
-          },
-        }])
-          .then(function (answers) {
-          // Set it:
-            script = answers.name;
-          });
-      case EXIT_SYMBOL:
-        return process.exit();
-      default:
-        script = answers.script;
+        case NEW_SCRIPT_SYMBOL:
+          // Get script name:
+          return inquirer
+            .prompt([
+              {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the script name:',
+                validate(val) {
+                  if (!val) return 'Script name must not be empty';
+                  return true;
+                },
+              },
+            ])
+            .then(answers => {
+              // Set it:
+              script = answers.name;
+            });
+        case EXIT_SYMBOL:
+          return process.exit();
+        default:
+          script = answers.script;
       }
     });
 }
 
 function confirmCreation() {
-  return inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'create',
-      message: `The script "${script}" does not exist. Create it?`,
-    },
-  ])
-    .then(function (answers) {
+  return inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'create',
+        message: `The script "${script}" does not exist. Create it?`,
+      },
+    ])
+    .then(answers => {
       if (!answers.create) {
         console.log('Aborting');
         process.exit();
@@ -136,9 +142,9 @@ function confirmCreation() {
 }
 
 function pad(str1, str2) {
-  var padLen = 60 - (str1.length + str2.length);
+  const padLen = 60 - (str1.length + str2.length);
   // Ensure at least one space:
-  var pad = ' ';
-  for (var i = 1; i < padLen; i++) pad += ' ';
+  let pad = ' ';
+  for (let i = 1; i < padLen; i++) pad += ' ';
   return str1 + pad + str2;
 }
